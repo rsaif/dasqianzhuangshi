8f5c0d63d7f0b68b8efb0dfd281929f36cdebe28
xhpmac <xhpmac@xhpmacdeMacBook-Pro.local>
2014-10-16 09:37:59 +0800

    创建学装修 的分类页面

---
+++ APP/ViewController/Learn/LearnCatViewController.h
@@ -0,0 +1,13 @@
+//
+//  LearnCatViewController.h
+//  Zhuangshi
+//
+//  Created by xhpmac on 14-10-16.
+//  Copyright (c) 2014年 wang shuguan. All rights reserved.
+//
+
+#import "ViewControllerImpl.h"
+
+@interface LearnCatViewController : ViewControllerImpl
+
+@end
---
+++ APP/ViewController/Learn/LearnCatViewController.m
@@ -0,0 +1,38 @@
+//
+//  LearnCatViewController.m
+//  Zhuangshi
+//
+//  Created by xhpmac on 14-10-16.
+//  Copyright (c) 2014年 wang shuguan. All rights reserved.
+//
+
+#import "LearnCatViewController.h"
+
+@interface LearnCatViewController ()
+
+@end
+
+@implementation LearnCatViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
